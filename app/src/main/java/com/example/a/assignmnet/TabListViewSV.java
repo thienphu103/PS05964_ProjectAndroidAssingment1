385e2771d8a7c10a6370f09a29ca5cc51f9b8442
thienphu103 <thienphu103@gmail.com>
2017-08-03 05:23:51 +0700

    update

--- app/src/main/java/com/example/a/assignmnet/TabListViewSV.java
+++ app/src/main/java/com/example/a/assignmnet/TabListViewSV.java
@@ -32,6 +32,7 @@ public class TabListViewSV extends Fragment {
     String mssvdialog;
     private Button btnTEST;
     Button btn;
+    Button btn1,btn2;
     ListView LvSinhVien;
     private ArrayList<SinhVien> arrayList;
     private AdapterSinhVien adapterSinhVien;
@@ -45,15 +46,18 @@ public class TabListViewSV extends Fragment {
         mssv = (EditText) view.findViewById(R.id.txtPassDialog);
         lvSinhVien = (ListView) view.findViewById(R.id.listViewSinhVien);
         arrayList = new ArrayList<>();
-        arrayList.clear();
+
         database = new SQLite(getContext(), "Student3.sqlite", null, 1);
         database.QueryData("CREATE TABLE IF NOT EXISTS Sinhvien(Id INTEGER PRIMARY KEY AUTOINCREMENT,Ten WARCHAR,MSSV WARCHAR)");
+//        database.QueryData("DELETE FROM Sinhvien "); (DELETE All)
         GetDataDB();
         adapterSinhVien = new AdapterSinhVien(getActivity(), R.layout.info_sv, arrayList);
 
         LvSinhVien.setAdapter(adapterSinhVien);
 
         btn = (Button) view.findViewById(R.id.btnAddStu);
+        btn1 = (Button) view.findViewById(R.id.btnDelStu);
+        btn2 = (Button) view.findViewById(R.id.btnUpStu);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,17 +67,16 @@ public class TabListViewSV extends Fragment {
                 user = (EditText) dialog.findViewById(R.id.txtUserDialog);
                 mssv = (EditText) dialog.findViewById(R.id.txtPassDialog);
                 btnadd = (Button) dialog.findViewById(R.id.btnAdd1);
+
                 btnadd.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
                         userdialog = user.getText().toString();
-                        mssvdialog = user.getText().toString();
+                        mssvdialog = mssv.getText().toString();
                         database.QueryData("INSERT INTO Sinhvien VALUES(null,'" + userdialog + "','" + mssvdialog + "')");
-                        arrayList.add(new SinhVien(userdb, mssvdb, 0));
                         Toast.makeText(getContext(), "Add OK", Toast.LENGTH_SHORT).show();
                         adapterSinhVien.notifyDataSetChanged();
                         dialog.dismiss();
-
                         GetDataDB();
 
                     }
@@ -82,11 +85,69 @@ public class TabListViewSV extends Fragment {
                 dialog.show();
             }
         });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog1 = new Dialog(getActivity());
+                dialog1.setContentView(R.layout.activity_dialog);
+                dialog1.setTitle("Delete Student");
+                user = (EditText) dialog1.findViewById(R.id.txtUserDialog);
+                user.setHint("Input Id Detete");
+                mssv = (EditText) dialog1.findViewById(R.id.txtPassDialog);
+                mssv.setVisibility(View.INVISIBLE);
+                btnadd = (Button) dialog1.findViewById(R.id.btnAdd1);
+                btnadd.setText("Delete");
+                btnadd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        userdialog = user.getText().toString();
+                        database.QueryData("DELETE FROM Sinhvien WHERE Ten='"+userdialog+"'");//Detete row name & id
+//                        database.QueryData("DELETE FROM Sinhvien");
+                        Toast.makeText(getContext(), "Delete OK", Toast.LENGTH_SHORT).show();
+                        adapterSinhVien.notifyDataSetChanged();
+                        dialog1.dismiss();
+                        GetDataDB();
+                    }
+                });
+                dialog1.show();
+
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Dialog dialog1 = new Dialog(getActivity());
+                dialog1.setContentView(R.layout.activity_dialog);
+                dialog1.setTitle("Update Student");
+                user = (EditText) dialog1.findViewById(R.id.txtUserDialog);
+                user.setHint("Input Id Update");
+                mssv = (EditText) dialog1.findViewById(R.id.txtPassDialog);
+                mssv.setHint("Update Here by Id");
+                btnadd = (Button) dialog1.findViewById(R.id.btnAdd1);
+                btnadd.setText("Update");
+                btnadd.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        userdialog = user.getText().toString();
+                        mssvdialog = mssv.getText().toString();
+                        database.QueryData("UPDATE Sinhvien SET Ten='"+mssvdialog+"' WHERE Ten='"+userdialog+"'");//Uodate
+//                        database.QueryData("DELETE FROM Sinhvien");
+                        Toast.makeText(getContext(), "Update OK", Toast.LENGTH_SHORT).show();
+                        adapterSinhVien.notifyDataSetChanged();
+                        dialog1.dismiss();
+                        GetDataDB();
+                    }
+                });
+                dialog1.show();
+
+            }
+        });
         return view;
     }
 
     final public void GetDataDB() {
-
+        arrayList.clear();
+//Get Database and display
         Cursor cursor = database.GetData("SELECT * FROM Sinhvien");
         while (cursor.moveToNext()) {
             userdb = cursor.getString(1);
@@ -95,6 +156,7 @@ public class TabListViewSV extends Fragment {
         }
 
     }
+
 
 
 }
